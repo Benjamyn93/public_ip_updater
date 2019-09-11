f5a10f0d8ca06fa083a11596923845bd3047831c
whitieman <benjamyn1993@gmail.com>
2019-09-11 23:23:31 +1000

    Update ExternalIP.py

--- ExternalIP.py
+++ ExternalIP.py
@@ -1,4 +1,8 @@
 import requests
+import re
 
 r = requests.get("https://www.ipchicken.com")
-print(r.content)
+b = r.content
+btostring = str(b)
+ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', btostring )
+print(ip)
