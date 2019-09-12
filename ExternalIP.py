5832c94c5b525c946afedbb1108c70d5a4dc33e0
Benjamyn Whiteman <benjamyn@Benjamyns-MacBook-Pro.local>
2019-09-12 22:59:54 +1000

    gui pop up box containing ip address added

--- ExternalIP.py
+++ ExternalIP.py
@@ -1,8 +1,12 @@
 import requests
 import re
+import subprocess
+import easygui
 
 r = requests.get("https://www.ipchicken.com")
 b = r.content
 btostring = str(b)
 ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', btostring )
-print(ip)
+#print(ip)
+
+easygui.buttonbox('This is your external IP Address', 'IP Address', (ip))
